3249e091340843b89c93c01f3eba2994e0fe4a2b
KK1XI <101323099+KK1XI@users.noreply.github.com>
2023-04-04 07:44:36 +0800

    Update project.py

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ class Project:
         self.description = description
         self.progress = progress
         self.dueDate = dueDate
+        self.subprojects = []
 
     def update(self, uid, name, description, progress, dueDate):
         self.uid = uid
@@ -12,3 +13,22 @@ class Project:
         self.description = description
         self.progress = progress
         self.dueDate = dueDate
+        
+    def add_subproject(self, subproject):
+        self.subprojects.append(subproject)
+        self.update_progress()
+
+    def remove_subproject(self, index):
+        self.subprojects.pop(index)
+        self.update_progress()
+
+    def update_progress(self):
+        if self.subprojects:
+            total_progress = sum([subproject.progress for subproject in self.subprojects])
+            self.progress = total_progress // len(self.subprojects)
+        else:
+            self.progress = 0
+
+    def update_subproject(self, index, progress):
+        self.subprojects[index].progress = progress
+        self.update_progress()
